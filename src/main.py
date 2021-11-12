5bdda5e66e1310c58dab719b87dfe7242cfecce9
Benjamin Mourgues <benja.mourgues@hotmail.fr>
2021-11-12 17:03:23 +0100

    Add basic template and JS libs

--- src/main.py
+++ src/main.py
@@ -1,8 +1,20 @@
 from flask import Flask
+from flask import render_template
+
+from stop_words import STOP_WORDS
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def landing_page():
-    return "<p>Hello, World!</p>"
+    context = {}
+    context["message"] = """Perdu ? Demandez votre chemain à GrandPybot,
+    le cyberpapy !"""
+    return render_template("home.html", **context)
 
+
+@app.route("/ajax/parser")
+def parser(request):
+    # TODO: Implement ajax parser
+    return render_template("home.html")
