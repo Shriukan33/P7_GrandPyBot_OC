3a0b8778c53b70254b373b918c27704896a760d3
Benjamin Mourgues <benja.mourgues@hotmail.fr>
2021-11-19 14:54:19 +0100

    Add Wikipedia API class
    The goal is to be able to query wikimedia_api and get an extract of the page
    if we get a match.

--- src/main.py
+++ src/main.py
@@ -7,6 +7,8 @@ from flask_wtf.csrf import CSRFProtect
 from settings_local import SECRET_KEY
 
 from message_handler import MessageHandler as mh
+from wikimedia_api import wikiAPI as wiki
+
 
 app = Flask(__name__)
 csrf = CSRFProtect(app)
@@ -25,13 +27,24 @@ def parser():
     data = request.form.to_dict()
 
     unparsed_message = data["unparsed_message"]
+
+    parsed_message = mh.parse_message(unparsed_message)
+
+    # TODO: Add call to Google maps API here
+
+    wiki_answer = wiki()
+    wiki_answer = wiki.get_wiki_answer(wiki_answer, parsed_message)
+    if wiki_answer:
+        bot_message = f"{parsed_message}, wiki dit : {wiki_answer['extract']}"
+    else:
+        bot_message = "Pardon, tu peux répéter ? Mon audition n'est plus" + \
+            " ce qu'elle était ..."
+
+    bot_anwser = mh.wrap_message(bot_message, is_bot=True)
+
     # Cleanse the message to avoid malicious code
     cleaned_message = clean(unparsed_message, strip=True)
     user_message = mh.wrap_message(cleaned_message, is_bot=False)
-
-    parsed_message = mh.parse_message(unparsed_message)
-    # TODO: Add a call to Wikimedia API to complete the answer
-    bot_anwser = mh.wrap_message(parsed_message, is_bot=True)
 
     context = {}
     # TODO: Keep more than one message in the history
---
+++ src/wikimedia_api.py
@@ -0,0 +1,105 @@
+import requests
+
+
+class wikiAPI:
+    """
+    This class groups the functions needed for the Wikimedia
+    API to be used.
+    Documentation is here:
+    https://www.mediawiki.org/wiki/API:Main_page
+    """
+
+    def __init__(self):
+        """
+        This function initializes the class.
+        """
+        self.api_url = "https://fr.wikipedia.org/w/api.php"
+
+    def get_page_info(self, query: str) -> dict:
+        """
+        This function gets the page info of the 1st hit of a
+        search on the Wikipedia API.
+        """
+        params = {
+            "action": "query",
+            "format": "json",
+            "list": "search",
+            "srsearch": query,
+            "srlimit": 1
+        }
+        response = requests.get(self.api_url, params=params)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            return None
+
+    def get_page_id(self, query: str) -> int:
+        """
+        This function gets the page id of the 1st hit of a
+        search on the Wikipedia API.
+        """
+        page_info = self.get_page_info(query)
+        if page_info is not None:
+            return page_info["query"]["search"][0]["pageid"]
+        else:
+            return None
+
+    def get_extract(self, page_id: int) -> dict:
+        """
+        This function gets the 3 first sentences of a page.
+        """
+        params = {
+            "action": "query",
+            "format": "json",
+            "formatversion": "latest",
+            "prop": "extracts",
+            "exsentences": 3,
+            "explaintext": True,
+            "pageids": page_id
+        }
+        response = requests.get(self.api_url, params=params)
+        if response.status_code == 200:
+            extract = response.json()
+            return extract["query"]["pages"][0]["extract"]
+        else:
+            return None
+
+    def get_page_url(self, page_id: int) -> str:
+        """
+        This function gets the url of a page.
+        """
+        params = {
+            "action": "query",
+            "format": "json",
+            "formatversion": "latest",
+            "prop": "info",
+            "inprop": "url",
+            "pageids": page_id
+        }
+        response = requests.get(self.api_url, params=params)
+        if response.status_code == 200:
+            url = response.json()
+            return url["query"]["pages"][0]["fullurl"]
+        else:
+            return None
+
+    def get_wiki_answer(self, query: str) -> dict:
+        """
+        This function gets the page info of the 1st hit of a
+        search on the Wikipedia API and the 3 first sentences of
+        the page.
+        """
+        try:
+            page_id = self.get_page_id(query)
+        except IndexError:
+            return None
+
+        if page_id is not None:
+            extract = self.get_extract(page_id)
+            url = self.get_page_url(page_id)
+            return {
+                "extract": extract,
+                "url": url
+            }
+        else:
+            return None
